a67af2356a080f9a7b3fd90cac70e5e53e650685
root <1323841527@qq.com>
2021-11-10 13:32:27 +0800

    改为原来的url

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from user.views import *
 urlpatterns = [
     path('register/',RegisterView.as_view(),name='register'),
-    path('',RegisterView.as_view(),name='rs'),
-    path("user/",RegisterView.as_view())
 ]
