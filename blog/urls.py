d5eae0577f89c48aba7eb91d0f80c5580e76934b
root <1323841527@qq.com>
2021-11-17 12:19:02 +0800

    完成

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from blog.views import *
 urlpatterns = [
     path('index/',IndexView.as_view(),name='index'),
     path("write/",WreitView.as_view(),name='write'),
+    path('detail/',DetailView.as_view(),name='detail')
 ]
